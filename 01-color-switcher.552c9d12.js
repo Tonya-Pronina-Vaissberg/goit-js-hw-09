4399504d028c7eafac344e2fd36e7ceaa5da4b07
Tonya-Pronina-Vaissberg <110049666+Tonya-Pronina-Vaissberg@users.noreply.github.com>
2023-04-25 17:26:33 +0000

    Deploying to gh-pages from @ Tonya-Pronina-Vaissberg/goit-js-hw-09@a63a22bb58fd0157f4f0f3d0197f026d13f11026 🚀

---
+++ 01-color-switcher.552c9d12.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");t.addEventListener("click",(function(){t.disabled=!0,n=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3)})),e.addEventListener("click",(function(){t.disabled=!1,clearInterval(n)}));var n=null}();
+//# sourceMappingURL=01-color-switcher.552c9d12.js.map
